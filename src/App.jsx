a500284be10ec953d68f26dfdd741ef8b236bd60
Somesh <someshdevera98@gmail.com>
2025-05-20 20:00:59 +0530

    add dist 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Banner from './assets/Components/Banner'
 import Header from './assets/Components/Header'
+// import MovieList from './assets/Components/Movielist'
 import MovieList from './assets/Components/Movielist'
 
 function App() {
