c662fb4d9a5bce403a7718ee050d02139bdb2577
Somesh <someshdevera98@gmail.com>
2025-05-20 20:02:49 +0530

    add dist 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Banner from './assets/Components/Banner'
 import Header from './assets/Components/Header'
 // import MovieList from './assets/Components/Movielist'
-import MovieList from './assets/Components/Movielist'
+import MovieList from './assets/Components/MovieList.jsx';
 
 function App() {
 
